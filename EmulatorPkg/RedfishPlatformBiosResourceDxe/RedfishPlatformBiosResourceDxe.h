8960a09ac0f0a3fc8e335670cf0b69f01fa57616
Abner Chang <abner.chang@hpe.com>
2020-03-19 10:48:52 +0800

    EmulatorPkg/RedfishPlatformBiosResourceDxe driver
    Install EFI_REDFISH_PLATFORM_BIOS_RESOURCE_PROTOCOL to provide platform level
    strategy of Redfish BIOS resource.
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>

---
+++ EmulatorPkg/RedfishPlatformBiosResourceDxe/RedfishPlatformBiosResourceDxe.h
@@ -0,0 +1,22 @@
+/** @file
+  Definition of Redfish Platform BIOS Resource DXE driver.
+
+  (C) Copyright 2020 Hewlett Packard Enterprise Development LP<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#ifndef EFI_REDFISH_PLATFORM_BIOS_RESOURCE_DXE_H_
+#define EFI_REDFISH_PLATFORM_BIOS_RESOURCE_DXE_H_
+
+#include <Protocol/RedfishPlatformBiosResource.h>
+
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/PrintLib.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+#endif
